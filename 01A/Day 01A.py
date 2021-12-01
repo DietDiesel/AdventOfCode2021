a1f15d2cc830eb4c4d4e774e81197876c6dfd21b
bgravett <bradgravett@gmail.com>
2021-12-01 14:14:11 -0600

    read lines out of input text file and load into list

--- 01A/Day 01A.py
+++
@@ -1,5 +0,0 @@
-
-
-puzzInput = "https://adventofcode.com/2021/day/1/input"
-print(puzzInput)
-print("getting py set uppp")
---
+++ 01A/day01a.py
@@ -0,0 +1,11 @@
+
+
+lines = []
+
+
+with open("puzzInput.txt", "rt") as f:
+    lines = f.readlines()
+
+print("item 0 in the puzz list is", lines[0])
+print("item 1 in the puzz list is", lines[1])
+print("item 2 in the puzz list is", lines[2])
