476cf2cb38a88338469fd09e1888d8bbc20cf986
swankidelic <swankidelic@gmail.com>
2021-12-02 16:37:07 -0600

    day 2 B complete - organization cleanup

--- day02b/day02b.py
+++ day02b/day02b.py
@@ -7,21 +7,19 @@ depth = 0
 hPosition = 0
 aim = 0
 
-
-
 for cmd in commands:
+
     if cmd[:2] == "fo":
         speed = int(cmd[8:9])
         hPosition = hPosition + speed
         depth = depth + (speed * aim)
-    elif cmd[:2] == "up":
-        speed = int(cmd[3:4])
-        aim = aim - speed
-    elif cmd[:2] == "do":
-        speed = int(cmd[5:6])
-        aim = aim + speed
 
-    
+    elif cmd[:2] == "up":
+        aim = aim - int(cmd[3:4])
+
+    elif cmd[:2] == "do":
+        aim = aim + int(cmd[5:6])
+
 print("hPosition", hPosition)
 print("depth", depth)
 print("answer", hPosition * depth)
