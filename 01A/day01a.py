2dd67f2bd2894a9cb36ccb294c7271fc6bfbf5e3
swankidelic <swankidelic@gmail.com>
2021-12-01 20:35:55 -0600

    day 1 A complete

--- 01A/day01a.py
+++ 01A/day01a.py
@@ -1,11 +1,21 @@
 
-
 lines = []
 
-
-with open("puzzInput.txt", "rt") as f:
+with open("01A\puzzInput.txt") as f:
     lines = f.readlines()
 
-print("item 0 in the puzz list is", lines[0])
-print("item 1 in the puzz list is", lines[1])
-print("item 2 in the puzz list is", lines[2])
+for i in lines:
+    i = int(i)  
+
+count = 0
+i = 0
+
+while i < len(lines) - 1:
+    if lines[i] < lines[i + 1]:
+        count = count + 1
+    i = i + 1
+
+# if lines[i] < lines[len(lines)]:
+#     count = count + 1
+
+print("final count is", count)
