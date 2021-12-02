2b5704d0cdcb5714394feef5c8f02974be905aaa
swankidelic <swankidelic@gmail.com>
2021-12-02 09:19:24 -0600

    day 2 A progress

--- 02A/day02a.py
+++ 02A/day02a.py
@@ -3,5 +3,8 @@ commands = []
 with open("02A\day2input.txt") as f:
     commands = f.readlines()
 
-print(commands[0][:-3])
-print(commands[0][-2:-1])
+print(int(commands[0][-2:-1]) + int(commands[1][-2:-1]))
+
+for cmd in commands:
+    print(cmd[:-3])
+    print(int(cmd[-2:-1]))
