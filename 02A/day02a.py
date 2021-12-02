86c0276ce9429e4864e1121383b9746603abea32
swankidelic <swankidelic@gmail.com>
2021-12-02 09:46:04 -0600

    day 2 A complete

--- 02A/day02a.py
+++ 02A/day02a.py
@@ -3,8 +3,23 @@ commands = []
 with open("02A\day2input.txt") as f:
     commands = f.readlines()
 
-print(int(commands[0][-2:-1]) + int(commands[1][-2:-1]))
+depth = 0
+hPosition = 0
+
+
 
 for cmd in commands:
-    print(cmd[:-3])
-    print(int(cmd[-2:-1]))
+    print("command", cmd[:2])
+    if cmd[:2] == "fo":
+        print("forward", cmd[8:9])
+        hPosition = hPosition + int(cmd[8:9])
+    elif cmd[:2]== "up":
+        print("up", cmd[3:4])
+        depth = depth - int(cmd[3:4])
+    elif cmd[:2]== "do":
+        print("down", cmd[5:6])
+        depth = depth + int(cmd[5:6])
+    
+print("hPosition", hPosition)
+print("depth", depth)
+print("answer", hPosition * depth)
