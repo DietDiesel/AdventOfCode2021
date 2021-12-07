06edd02f2a664694a006d20c52a99eef8d572cc0
swankidelic <swankidelic@gmail.com>
2021-12-07 00:05:44 -0600

    still plugging away at day 3

--- 03B/day03b.py
+++ 03B/day03b.py
@@ -1,24 +1,37 @@
-
-diag = []
-
 with open("03A\day03input.txt") as f:
     diag = f.readlines()
 
-targetList = []
+for row in diag: # Trim off the line break character at the end of each row
+    row = row[0:12]
 
-for col in diag[0]:
-    for row in diag:
-        
-        onesList = []
-        zerosList = []
-        
-        if row[col:col + 1] == 1:
-            onesList.append(row)
-        else:
-            zerosList.append(row)
+onesList = diag
+zerosList = diag
+col = 0
 
-    if len(onesList) > len(zerosList):
-        targetList = onesList
-    else:
-        targetList = zerosList
+while col < (len(diag[0]) - 1):
 
+    oxygenList = [] # Reset the target lists to be ready to received sorted rows
+    scrubberList = []
+
+    for row in diag: # First, add up all the ones in a column to facilitate the next step
+        total = total + int(row[col:col + 1])
+
+    if total > (len(diag) /2): # Use that total to see if we're adding one-columns to the output lists, or not
+        for row in onesList:
+            if row[col:col + 1] == "1": # Copy row to the target oxygenList if the current column has a 1
+                oxygenList.append(row)
+
+
+#
+#
+#
+#
+    
+    onesList = oxygenList # Bump the target list we just sorted up to the source list, ready for the next iteration
+    zerosList = scrubberList
+    col += 1
+
+# print(int(oxygenList[0],2))
+# print(int(scrubberList[0],2))
+print(len(oxygenList))
+print(len(scrubberList))
---
+++ 03B/sliceloop.py
@@ -0,0 +1,6 @@
+str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+i = 0
+while i < len(str):
+    print("iteration:",i)
+    print(str[i:i+2])
+    i += 1
